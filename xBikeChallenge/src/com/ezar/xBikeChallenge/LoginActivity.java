82e0e274d5e6b4e16034c4ff348494749e1f990f
Artur Rymarz <artur.rymarz@gmail.com>
2014-06-03 16:46:22 +0200

    Facebook Login

--- xBikeChallenge/src/com/ezar/xBikeChallenge/LoginActivity.java
+++ xBikeChallenge/src/com/ezar/xBikeChallenge/LoginActivity.java
@@ -1,15 +1,85 @@
 package com.ezar.xBikeChallenge;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import com.facebook.Session;
+import com.facebook.SessionState;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
 
 public class LoginActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    private LoginButton loginBtn;
+
+    private UiLifecycleHelper uiHelper;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
+
+        uiHelper = new UiLifecycleHelper(this, statusCallback);
+        uiHelper.onCreate(savedInstanceState);
+
+        loginBtn = (LoginButton) findViewById(R.id.fb_login_button);
+        loginBtn.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+            @Override
+            public void onUserInfoFetched(GraphUser user) {
+                if (user != null) {
+                    //userName.setText("Hello, " + user.getName());
+                } else {
+                    //userName.setText("You are not logged");
+                }
+            }
+        });
     }
+
+    private Session.StatusCallback statusCallback = new Session.StatusCallback() {
+        @Override
+        public void call(Session session, SessionState state,
+                         Exception exception) {
+            if (state.isOpened()) {
+                //buttonsEnabled(true);
+                Log.d("FacebookSampleActivity", "Facebook session opened");
+            } else if (state.isClosed()) {
+                //buttonsEnabled(false);
+                Log.d("FacebookSampleActivity", "Facebook session closed");
+            }
+        }
+    };
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        uiHelper.onResume();
+        //buttonsEnabled(Session.getActiveSession().isOpened());
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        uiHelper.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        uiHelper.onDestroy();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        uiHelper.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedState) {
+        super.onSaveInstanceState(savedState);
+        uiHelper.onSaveInstanceState(savedState);
+    }
+
 }
